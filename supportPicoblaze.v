936c378542892c4c422a1b18951408bf30791ac1
Scott Teal <scott@teals.org>
2015-08-25 11:20:46 -0400

    Moved support code to submodule "supportPicoblaze"

---
+++ supportPicoblaze.v
@@ -0,0 +1,173 @@
+
+///////////////////////////////////////////////////////////////////////////
+// MODULE DECLARATION
+///////////////////////////////////////////////////////////////////////////
+
+module supportPicoblaze
+#(
+    parameter PICO_IN_HIGH = 1,
+    parameter PICO_OUT_HIGH = 1
+)
+(
+    // INPUTS
+    input clk,               ///< System Clock
+    input nRst,              ///< Reset, active low
+    input cclk,              ///< Configuration Clock from AVR
+    input interrupt,         ///< Interrupt Picoblaze
+    input sleep,             ///< Put Picoblaze to sleep
+    input avrTx,             ///< UART RX from AVR
+    input avrRxBusy,         ///< RX is busy (from AVR)
+    input [7:0] inPort,      ///< Data to read in
+    input [7:0] uartTxData,  ///< Data sent to UART (should come from outPort)
+    input [1:0] uartControl, ///< UART control bits (should come from outPort)
+    // OUTPUTS
+    output [7:0] outPort,    ///< Data to write out
+    output [7:0] portId,     ///< Port being used
+    output [7:0] uartStatus, ///< UART status bits (should go to inPort)
+    output [7:0] uartRxData, ///< Data received from UART (should go to inPort)
+    output readStrobe,       ///< Picoblaze read strobe
+    output writeStrobe,      ///< Picoblaze write strobe
+    output kWriteStrobe,     ///< Picoblaze constant port write strobe
+    output rst,              ///< Active high, set by nRst and cclk status
+    output interruptAck,     ///< Interrupt ack'd by Picoblaze
+    output avrRx             ///< UART TX from AVR
+);
+
+///////////////////////////////////////////////////////////////////////////
+// SIGNAL DECLARATIONS
+///////////////////////////////////////////////////////////////////////////
+
+wire [11:0] address;
+wire [17:0]	instruction;
+wire avrReady; // High when AVR is ready to communicate
+wire bramEnable;
+wire uartRXFull;
+wire uartRxDataPresent;
+wire uartRxHalfFull;
+wire uartRxReset;
+wire uartTxDataPresent;
+wire uartTxFull;
+wire uartTxHalfFull;
+wire uartTxReset;
+
+reg [2:0] baudCount;
+reg [1:0] baudModulate;
+reg en16XBaud;
+reg readFromUartRx;
+reg writeToUartTx;
+
+///////////////////////////////////////////////////////////////////////////
+// SUPPORT CODE - Picoblaze & AVR Interface
+///////////////////////////////////////////////////////////////////////////
+
+assign rst = ~nRst | ~avrReady;
+
+// Wait for AVR to finish booting before running normally
+cclkDetector #(
+    .CLK_FREQ(50000000)
+)
+cclkDetector0
+(
+    .clk(clk),       ///< System Clock
+    .cclk(cclk),     ///< cclk input from AVR
+    .ready(avrReady) ///< when 1, the AVR is ready
+);
+
+kcpsm6 #(
+    .interrupt_vector(12'h000),
+    .scratch_pad_memory_size(64),
+    .hwbuild(8'h42)            // 42 hex is ASCII Character "B"
+) processor (
+    .address(address),
+    .instruction(instruction),
+    .bram_enable(bramEnable),
+    .port_id(portId),
+    .write_strobe(writeStrobe),
+    .k_write_strobe(kWriteStrobe),
+    .out_port(outPort),
+    .read_strobe(readStrobe),
+    .in_port(inPort),
+    .interrupt(interrupt),
+    .interrupt_ack(interruptAck),
+    .reset(rst),
+    .sleep(sleep),
+    .clk(clk)
+); 
+
+program programRom (
+    .enable(bramEnable),
+    .address(address),
+    .instruction(instruction),
+    .clk(clk)
+);
+
+uart_tx6 tx (
+    .data_in(uartTxData),
+    .en_16_x_baud(en16XBaud),
+    .serial_out(avrRx),
+    .buffer_write(writeToUartTx),
+    .buffer_data_present(uartTxDataPresent),
+    .buffer_half_full(uartTxHalfFull),
+    .buffer_full(uartTxFull),
+    .buffer_reset(uartTxReset),              
+    .clk(clk)
+);
+
+uart_rx6 rx (
+    .serial_in(avrTx),
+    .en_16_x_baud(en16XBaud),
+    .data_out(uartRxData),
+    .buffer_read(readFromUartRx),
+    .buffer_data_present(uartRxDataPresent),
+    .buffer_half_full(uartRxHalfFull),
+    .buffer_full(uartRXFull),
+    .buffer_reset(uartRxReset),              
+    .clk(clk)
+);
+
+// Set serial rate at 500000 with pulse at 8MHz. 50Mhz / 8 MHz = 6.25, so pulse at rate
+// 6...6...6...7 clocks.
+always @ (posedge clk )
+begin
+    if (rst) begin
+        baudCount    <= 'd0;
+        baudModulate <= 'd0;
+        en16XBaud    <= 1'b0;
+    end
+    else begin
+        if (baudCount == 3'd0) begin // counts 6 states including zero
+            if (baudModulate == 2'b11) begin
+                baudCount <= 3'd6;
+            end
+            else begin
+                baudCount <= 3'd5;
+            end
+            baudModulate <= baudModulate + 2'd1;
+            en16XBaud <= 1'b1;                 // single cycle enable pulse
+        end
+        else begin
+            baudCount <= baudCount - 3'd1;
+            en16XBaud <= 1'b0;
+        end
+    end
+end
+
+always @(posedge clk)
+begin
+    readFromUartRx <= readStrobe  && (portId[PICO_IN_HIGH:0]  ==  'd1);
+    writeToUartTx  <= writeStrobe && (portId[PICO_OUT_HIGH:0] ==  'd0);
+end
+
+assign uartStatus[7] = 1'b0;
+assign uartStatus[6] = avrRxBusy;
+assign uartStatus[5] = uartRXFull;
+assign uartStatus[4] = uartRxHalfFull;
+assign uartStatus[3] = uartRxDataPresent;
+assign uartStatus[2] = uartTxFull; 
+assign uartStatus[1] = uartTxHalfFull;
+assign uartStatus[0] = uartTxDataPresent;
+
+assign uartTxReset = uartControl[0];
+assign uartRxReset = uartControl[1];
+
+endmodule
--- topChip.v
+++ topChip.v
@@ -1,7 +1,7 @@
 
 
 ///////////////////////////////////////////////////////////////////////////
-// MODULE DECLARATION
+// MODULE DECLARATION {{{
 ///////////////////////////////////////////////////////////////////////////
 
 module topChip (
@@ -26,52 +26,39 @@ module topChip (
     output reg [7:0] led         ///< Outputs to the 8 onboard LEDs
     );
 
+// }}}
 ///////////////////////////////////////////////////////////////////////////
-// SIGNAL DECLARATIONS
+// PARAMETER DECLARATIONS {{{
 ///////////////////////////////////////////////////////////////////////////
 
 localparam PICO_IN_HIGH   = 1; ///< Highest bit used for Picoblaze input multiplexer
 localparam PICO_OUT_HIGH  = 1; ///< Highest bit used for Picoblaze output registers
 localparam PICO_KOUT_HIGH = 0; ///< Highest bit used for Picoblaze k_output registers
 
+// }}}
 ///////////////////////////////////////////////////////////////////////////
-// SIGNAL DECLARATIONS
+// SIGNAL DECLARATIONS {{{
 ///////////////////////////////////////////////////////////////////////////
 
-reg  [7:0] inPort;
-reg [1:0] baudModulate;
-reg [2:0] baudCount;
-reg [7:0] uartTxDataIn;
-reg en16XBaud;
-reg readFromUartRx;
-reg uartRxReset;
-reg uartTxReset;
-reg writeToUartTx;
-
-wire [11:0] address;
-wire [17:0]	instruction;
-wire [7:0]  uartRxDataOut;
 wire [7:0] outPort;
 wire [7:0] portId;
-wire avrReady; // High when AVR is ready to communicate
-wire bramEnable;
-wire interrupt;   
-wire interruptAck;
-wire kWriteStrobe;
-wire kcpsm6Reset;
-wire kcpsm6Sleep;  
+wire [7:0] uartStatus;
+wire [7:0] uartRxData;
 wire readStrobe;
-wire rst; // Reset, active high
-wire uartRXFull;
-wire uartRxDataPresent;
-wire uartRxHalfFull;
-wire uartTxDataPresent;
-wire uartTxFull;
-wire uartTxHalfFull;
 wire writeStrobe;
+wire kWriteStrobe;
+wire rst;
+wire interruptAck;
+wire interrupt;
+wire sleep;
 
+reg [7:0] inPort;
+reg [7:0] uartTxData;
+reg [1:0] uartControl;
+
+// }}}
 ///////////////////////////////////////////////////////////////////////////
-// SUPPORT CODE - Picoblaze & AVR Interface
+// SUPPORT CODE - Picoblaze & AVR Interface {{{
 ///////////////////////////////////////////////////////////////////////////
 
 // these signals should be high-z when not used
@@ -81,149 +68,74 @@ always @(*) begin
     spiChannel = 4'bzzzz;
 end
 
-assign rst = ~nRst | ~avrReady;
-
-// Wait for AVR to finish booting before running normally
-cclkDetector #(
-    .CLK_FREQ(50000000)
-)
-cclkDetector0
-(
-    .clk(clk),       ///< System Clock
-    .cclk(cclk),     ///< cclk input from AVR
-    .ready(avrReady) ///< when 1, the AVR is ready
-);
-
-assign kcpsm6Reset = rst;
-assign kcpsm6Sleep = 1'b0;
+assign sleep = 1'b0;
 assign interrupt = interruptAck;
-  
-kcpsm6 #(
-    .interrupt_vector(12'h000),
-    .scratch_pad_memory_size(64),
-    .hwbuild(8'h42)            // 42 hex is ASCII Character "B"
-) processor (
-    .address(address),
-    .instruction(instruction),
-    .bram_enable(bramEnable),
-    .port_id(portId),
-    .write_strobe(writeStrobe),
-    .k_write_strobe(kWriteStrobe),
-    .out_port(outPort),
-    .read_strobe(readStrobe),
-    .in_port(inPort),
-    .interrupt(interrupt),
-    .interrupt_ack(interruptAck),
-    .reset(kcpsm6Reset),
-    .sleep(kcpsm6Sleep),
-    .clk(clk)
-); 
 
-program programRom (
-    .enable(bramEnable),
-    .address(address),
-    .instruction(instruction),
-    .clk(clk)
+supportPicoblaze #(
+    .PICO_IN_HIGH(PICO_IN_HIGH),
+    .PICO_OUT_HIGH(PICO_OUT_HIGH)
+)
+supportPicoblaze0 (
+    // INPUTS
+    .clk(clk),                   ///< System Clock
+    .nRst(nRst),                 ///< Reset, active low
+    .cclk(cclk),                 ///< Configuration Clock from AVR
+    .interrupt(interrupt),       ///< Interrupt Picoblaze
+    .sleep(sleep),               ///< Put Picoblaze to sleep
+    .avrTx(avrTx),               ///< UART RX from AVR
+    .avrRxBusy(avrRxBusy),       ///< RX is busy (from AVR)
+    .inPort(inPort),             ///< [7:0] Data to read in
+    .uartTxData(uartTxData),     ///< [7:0] Data sent to UART (should come from outPort)
+    .uartControl(uartControl),   ///< [1:0] UART control bits (should come from outPort)
+    // OUTPUTS
+    .outPort(outPort),           ///< [7:0] Data to write out
+    .portId(portId),             ///< [7:0] Port being used
+    .uartStatus(uartStatus),     ///< [7:0] UART status bits (should go to inPort)
+    .uartRxData(uartRxData),     ///< [7:0] Data received from UART (should go to inPort)
+    .readStrobe(readStrobe),     ///< Picoblaze read strobe
+    .writeStrobe(writeStrobe),   ///< Picoblaze write strobe
+    .kWriteStrobe(kWriteStrobe), ///< Picoblaze constant port write strobe
+    .rst(rst),                   ///< Active high, set by nRst and cclk status
+    .interruptAck(interruptAck), ///< Interrupt ack'd by Picoblaze
+    .avrRx(avrRx)                ///< UART TX from AVR
 );
 
-uart_tx6 tx (
-    .data_in(uartTxDataIn),
-    .en_16_x_baud(en16XBaud),
-    .serial_out(avrRx),
-    .buffer_write(writeToUartTx),
-    .buffer_data_present(uartTxDataPresent),
-    .buffer_half_full(uartTxHalfFull),
-    .buffer_full(uartTxFull),
-    .buffer_reset(uartTxReset),              
-    .clk(clk)
-);
-
-uart_rx6 rx (
-    .serial_in(avrTx),
-    .en_16_x_baud(en16XBaud),
-    .data_out(uartRxDataOut),
-    .buffer_read(readFromUartRx),
-    .buffer_data_present(uartRxDataPresent),
-    .buffer_half_full(uartRxHalfFull),
-    .buffer_full(uartRXFull),
-    .buffer_reset(uartRxReset),              
-    .clk(clk)
-);
-
-// Set serial rate at 500000 with pulse at 8MHz. 50Mhz / 8 MHz = 6.25, so pulse at rate
-// 6...6...6...7 clocks.
-always @ (posedge clk )
-begin
-    if (rst) begin
-        baudCount    <= 'd0;
-        baudModulate <= 'd0;
-        en16XBaud    <= 1'b0;
-    end
-    else begin
-        if (baudCount == 3'd0) begin // counts 6 states including zero
-            if (baudModulate == 2'b11) begin
-                baudCount <= 3'd6;
-            end
-            else begin
-                baudCount <= 3'd5;
-            end
-            baudModulate <= baudModulate + 2'd1;
-            en16XBaud <= 1'b1;                 // single cycle enable pulse
-        end
-        else begin
-            baudCount <= baudCount - 3'd1;
-            en16XBaud <= 1'b0;
-        end
-    end
-end
-
+// }}}
 ///////////////////////////////////////////////////////////////////////////
-// PICOBLAZE I/O
+// PICOBLAZE I/O {{{
 ///////////////////////////////////////////////////////////////////////////
-
 
 // Input multiplexer
 always @ (posedge clk)
 begin
     inPort <= 'd0; // Default, to catch unset bits
     case (portId[PICO_IN_HIGH:0]) 
-        // Read UART status at port address 00 hex
-        'd0 : begin
-            inPort[5] <= uartRXFull;
-            inPort[4] <= uartRxHalfFull;
-            inPort[3] <= uartRxDataPresent;
-            inPort[2] <= uartTxFull; 
-            inPort[1] <= uartTxHalfFull;
-            inPort[0] <= uartTxDataPresent;
-        end
-        // Read UART_RX6 data at port address 01 hex
-        // (see 'bufferRead' pulse generation below) 
-        'd1 : inPort <= uartRxDataOut;
+        'd0 : inPort <= uartStatus; // Read UART status at port address 00 hex
+        'd1 : inPort <= uartRxData; // Read UART data at port address 01 hex
         default : inPort <= 8'd0;
     endcase
 
-    // Input Strobes
-    readFromUartRx <= readStrobe && (portId[PICO_IN_HIGH:0] == 1'b1);
+    // Read strobes
+    // exampleReadStrobe <= readStrobe & (portId[PICO_IN_HIGH:0] == PORT_NUM);
 end
 
 // Output Registers
 always @ (posedge clk)
 begin
     if (rst) begin
-        uartTxDataIn  <= 'd0;
-        led           <= 'd0;
-        writeToUartTx <= 1'b0;
+        uartTxData <= 'd0;
+        led        <= 'd0;
     end
     else begin
         if (writeStrobe) begin
             case (portId[PICO_OUT_HIGH:0])
-                'd0 : uartTxDataIn <= outPort;
-                'd1 : led          <= outPort;
+                'd0 : uartTxData <= outPort;
+                'd1 : led        <= outPort;
             endcase
         end
 
-        // Output Strobes
-        writeToUartTx <= writeStrobe &&  (portId[7:0] == 8'd0);
+        // Write strobes
+        // exampleWriteStrobe <= writeStrobe & (portId[PICO_OUT_HIGH:0] == PORT_NUM);
     end
 end
 
@@ -231,23 +143,22 @@ end
 always @ (posedge clk)
 begin
     if (rst) begin
-        uartTxReset <= 1'b1;
-        uartRxReset <= 1'b1;
+        uartControl <= 'd0;
     end
     else begin
         if (kWriteStrobe) begin
             case (portId[PICO_KOUT_HIGH:0])
-                'd0 : begin
-                    uartTxReset <= outPort[0];
-                    uartRxReset <= outPort[1];
-                end
+                'd0 : uartControl <= outPort[1:0];
             endcase
         end
     end
 end
 
+// }}}
 ///////////////////////////////////////////////////////////////////////////
-// MAIN CODE HERE
+// MAIN CODE HERE {{{
 ///////////////////////////////////////////////////////////////////////////
 
+// }}}
 endmodule
+/* vim: set fdm=marker: */
